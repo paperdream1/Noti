c0212a1ab299f090e86ad634ad091f82c69d217f
황정보 <dev.jeongbo@gmail.com>
2016-11-01 16:01:34 +0900

    add class
    add messege, schedule class

---
+++ Noti/Schedule.swift
@@ -0,0 +1,19 @@
+//
+//  Schedule.swift
+//  Noti
+//
+//  Created by 황정보 on 2016. 11. 1..
+//  Copyright © 2016년 이채원. All rights reserved.
+//
+
+import Foundation
+
+class Schedule {
+    var date:Date?
+    var content:String?
+    
+    init (date:Date, content:String){
+        self.date = date
+        self.content = content
+    }
+}
